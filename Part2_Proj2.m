f8c5a0773459e2d37d523cdd9808c21ac503bd3f
Tanveer Salim <Tanveer.Salim@protonmail.com>
2020-05-02 19:48:04 -0500

    Updating Part2_Proj.m and Part2_Proj2.docx

---
+++ Part2_Proj2.m
@@ -0,0 +1,13 @@
+Omega = linspace(-pi,pi,400);
+H = tf([1],[1 -0.5],-1);
+H_Omega = squeeze(freqresp(H,Omega));
+subplot(2,1,1);
+plot(Omega,abs(H_Omega),'k');
+axis tight;
+xlabel('\Omega');
+ylabel('H[e^{j \Omega}]|');
+subplot(2,1,2);
+plot(Omega,angle(H_Omega)*180/pi,'k');
+axis tight;
+xlabel('\Omega');
+ylabel('\angle H{e^{j \Omega}] [deg]');
